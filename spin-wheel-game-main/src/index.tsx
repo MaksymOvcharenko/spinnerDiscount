128146ee94f7f7e63d678d6ea7520f07c1ad0f3f
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-02-28 13:50:35 +0100

    new

--- spin-wheel-game-main/src/index.tsx
+++ spin-wheel-game-main/src/index.tsx
@@ -1,5 +1,4 @@
-import { ISegments, ISpinWheelProps } from "./components/SpinWheel.interface"
-import SpinWheel from "./components/SpinWheel"
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import DiscountBox from "./components/Spin";
@@ -10,9 +9,7 @@ import DiscountBox from "./components/Spin";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     
   <React.StrictMode>
-        {/* <MySpinWheel/> */}
+       
         <DiscountBox/>
   </React.StrictMode>
 );
-export type { ISegments, ISpinWheelProps }
-export { SpinWheel }
